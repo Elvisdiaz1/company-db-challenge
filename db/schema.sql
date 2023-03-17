78581978d99336aaa520654e99add740681ff4f2
Elvis Diaz <elvisdiaz020@gmail.com>
2023-03-16 22:22:50 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+ DROP DATABASE IF EXISTS company_db;
+ CREATE DATABASE company_db;
+
+ Use company_db;
+
+ CREATE TABLE department ();
+
+ CREATE TABLE role();
+
+ CREATE TABLE employee();
+
+ CREATE TABLE manager();
